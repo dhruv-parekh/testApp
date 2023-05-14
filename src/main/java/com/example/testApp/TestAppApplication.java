acfe0f9d381d92849ae086186992d05fbc2b2198
dhruvparekh-admin <contact.dparekh@gmail.com>
2023-05-14 16:03:24 -0400

    second commit

--- src/main/java/com/example/testApp/TestAppApplication.java
+++ src/main/java/com/example/testApp/TestAppApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TestAppApplication.class, args);
+		System.out.println("hello world");
 	}
+
 
 }
