f06b48c7891bab88c2badd8a1c15201b9c12eec9
dhruvparekh-admin <contact.dparekh@gmail.com>
2023-05-17 18:18:48 -0400

    Validations and Exceptions

--- src/main/java/com/example/testApp/TestAppApplication.java
+++ src/main/java/com/example/testApp/TestAppApplication.java
@@ -2,9 +2,6 @@ package com.example.testApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
